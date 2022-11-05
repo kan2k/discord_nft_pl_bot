98ae5e67d5bf2767ecd4c957e78e0a9272069a7a
kan2k <jkwokchunkan@gmail.com>
2022-11-06 05:48:52 +0800

    added wallet saving feature

--- discord_bot.py
+++ discord_bot.py
@@ -6,6 +6,33 @@ from dotenv import dotenv_values
 import discord, os, json, i18n, typing, functools
 from io import BytesIO
 
+from sqlalchemy import create_engine, Column, String, Integer, CHAR, PickleType
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.mutable import MutableDict
+from sqlalchemy.orm import sessionmaker
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "wallets"
+    discord_id = Column("discord_id", Integer, primary_key=True)
+    profile = Column("profile", MutableDict.as_mutable(PickleType))
+
+    def __init__(self, discord_id, profile):
+        self.discord_id = discord_id
+        self.profile = profile
+    def __repr__(self):
+        return f"{self.discord_id} {self.profile}"
+
+engine = create_engine("sqlite:///" + os.path.join(here, "database.db"))
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
 here = os.path.dirname(os.path.abspath(__file__))
 config = dotenv_values(os.path.join(here, ".env"))
 
@@ -54,12 +81,112 @@ async def on_guild_join(guild):
     default_settings = {"language": "en", "eth_decimal": 3, "brand_image": "", "template": "", "font": "", "bold_font": "", "magic": []}
     set_settings(str(guild.id), default_settings)
 
-@bot.tree.command(name="profit")
-@app_commands.describe(collection="contract address or Opensea URL", wallet_addresses="Wallet Address")
-async def profit(interaction: discord.Integration, collection: str, wallet_addresses: str):
+@bot.tree.command(name="wallet")
+@app_commands.describe(action="action", profile="profile", wallets="wallets")
+async def profit(interaction: discord.Integration, action: str, profile: str=None, wallets: str=None):
     await interaction.response.defer(ephemeral=True)
-    user = await bot.fetch_user(interaction.user.id)
-    print(f"> [{interaction.guild.name}] {interaction.user.name} requested {collection} for wallets {wallet_addresses}")
+    discord_user = await bot.fetch_user(interaction.user.id)
+    print(f"> [{interaction.guild.name}] {interaction.user.name} {action} {profile} {wallets}")
+    settings = get_settings(interaction.guild_id)
+    i18n.set('locale', settings['language'])
+    user = session.query(User).get(discord_user.id)
+    if action.lower() == 'view':
+        if not profile:
+            embed = discord.Embed(title=f"{i18n.t('wallet_embed_title', discord_name=discord_user.name)}", description=f"", color=discord.Colour.purple())
+            embed.set_footer(text="Powered by https://twitter.com/Jaasonft", icon_url="https://i.imgur.com/6ZaSEwK.png")
+            wallet_message = ""
+            for profile_key in user.profile.keys():
+                wallet_count = len(user.profile[profile_key])
+                wallet_message += f"`{profile_key} ({wallet_count} wallets)` "
+            embed.add_field(name=f"{i18n.t('wallet_profile')}", value=f"{wallet_message}")
+            embed.set_thumbnail(url=settings['brand_image'])
+            await interaction.followup.send(embed=embed, ephemeral=True)
+        else:
+            embed = discord.Embed(title=f"{i18n.t('wallet_embed_title', discord_name=discord_user.name)}", description=f"", color=discord.Colour.purple())
+            embed.set_footer(text="Powered by https://twitter.com/Jaasonft", icon_url="https://i.imgur.com/6ZaSEwK.png")
+            wallet_message = ""
+            try:
+                for wallet in user.profile[profile]:
+                    wallet_message += f"`{wallet}`\n"
+            except:
+                await interaction.followup.send(f"{i18n.t('profile_does_not_exist', profile=profile)}", ephemeral=True)
+                return
+            wallet_message += f"\n`{i18n.t('total_wallets', count=len(user.profile[profile]))}`"
+            embed.add_field(name=f"{i18n.t('wallets_for_profile', profile=profile)}", value=f"{wallet_message}")
+            embed.set_thumbnail(url=settings['brand_image'])
+            await interaction.followup.send(embed=embed, ephemeral=True)
+        return
+    elif action.lower() == 'add':
+        if not profile or not wallets:
+            await interaction.followup.send(f"{i18n.t('missing_parameter')}", ephemeral=True)
+            return
+        
+        try:
+            wallets = clean_wallets(wallets.split(" "))
+        except Exception as e:
+            await interaction.followup.send(f"{i18n.t('invalid_wallet', wallet=e)}", ephemeral=True)
+            return
+
+        if user:
+            try:
+                # profile wallet edit
+                user.profile[profile] = list(set(user.profile[profile] + wallets))
+                await interaction.followup.send(f"{i18n.t('profile_edited', profile=profile)}", ephemeral=True)
+            except:
+                # profile wallet add
+                user.profile[profile] = wallets
+                print(user.profile[profile])
+                await interaction.followup.send(f"{i18n.t('profile_created', profile=profile)}", ephemeral=True)
+        else:
+            # profile wallet create
+            new_profile = {}
+            new_profile[profile] = wallets
+            new_user = User(discord_user.id, new_profile)
+            session.add(new_user)
+            await interaction.followup.send(f"{i18n.t('profile_created', profile=profile)}", ephemeral=True)
+
+    elif action.lower() == 'delete':
+        if not profile and not wallets:
+            await interaction.followup.send(f"{i18n.t('missing_parameter')}", ephemeral=True)
+            return
+
+        if profile and not wallets:
+            try:
+                del user.profile[profile]
+            except:
+                await interaction.followup.send(f"{i18n.t('profile_does_not_exist', profile=profile)}", ephemeral=True)
+                return
+            await interaction.followup.send(f"{i18n.t('deleted_profile', profile=profile)}", ephemeral=True)
+            return
+
+        try:
+            wallets = clean_wallets(wallets.split(" "))
+        except Exception as e:
+            await interaction.followup.send(f"{i18n.t('invalid_wallet', wallet=e)}", ephemeral=True)
+            return
+
+        if profile and wallets:
+            message = ""
+            for wallet in wallets:
+                try:
+                    user.profile[profile].remove(wallet)
+                    message += f"`{wallet}` "
+                except:
+                    continue
+            await interaction.followup.send(f"{i18n.t('deleted_wallets_profile', message=message, profile=profile)}", ephemeral=True)
+    try:
+        session.commit()
+    except Exception as e:
+        print("DATABASE ERROR:",e)
+        session.rollback()
+        
+
+@bot.tree.command(name="profit")
+@app_commands.describe(collection="contract address or Opensea URL", wallet_profile="Wallet Profile")
+async def profit(interaction: discord.Integration, collection: str, wallet_profile: str):
+    await interaction.response.defer(ephemeral=True)
+    discord_user = await bot.fetch_user(interaction.user.id)
+    print(f"> [{interaction.guild.name}] {interaction.user.name} requested {collection} for wallets {wallet_profile}")
     settings = get_settings(interaction.guild_id)
     i18n.set('locale', settings['language'])
 
@@ -67,17 +194,16 @@ async def profit(interaction: discord.Integration, collection: str, wallet_addre
         await interaction.followup.send(f"{i18n.t('invalid_collection', collection=collection)}", ephemeral=True)
         return
 
-    clean_wallets = []
-    for wallet in wallet_addresses.split(" "):
-        if len(wallet) != 42 or wallet[:2] != "0x":
-            await interaction.followup.send(f"{i18n.t('invalid_wallet', wallet=wallet)}", ephemeral=True)
-            return
-        if wallet not in clean_wallets:
-            clean_wallets.append(wallet.lower())
-    
+    user = session.query(User).get(discord_user.id)
+    try:
+        wallets = user.profile[wallet_profile]
+    except:
+        await interaction.followup.send(f"{i18n.t('invalid_profile', profile=wallet_profile)}", ephemeral=True)
+        return
+
     try:
         # data = await run_blocking(get_pl, collection, clean_wallets)
-        data = await get_pl(collection, clean_wallets)
+        data = await get_pl(collection, wallets)
     except Exception as e:
         print(e)
         await interaction.followup.send(f"{i18n.t('data_error')}", ephemeral=True)
@@ -85,7 +211,7 @@ async def profit(interaction: discord.Integration, collection: str, wallet_addre
 
     eth_decimal = settings['eth_decimal']
 
-    message_1 = i18n.t('amount_of_wallets', total_wallets=len(clean_wallets))
+    message_1 = i18n.t('amount_of_wallets', total_wallets=len(wallets))
     message_2 = ""
 
     if data['break_even_amount'] > 0:
@@ -121,7 +247,7 @@ async def profit(interaction: discord.Integration, collection: str, wallet_addre
         await interaction.followup.send(embed=embed, ephemeral=True)
     else:
         bytes = BytesIO()
-        img = profit_image(user, data, settings)
+        img = profit_image(discord_user, data, settings)
         img.save(bytes, 'jpeg', quality=100)
         bytes.seek(0)
         image = discord.File(bytes, filename="image.jpg")
@@ -132,6 +258,18 @@ async def profit(interaction: discord.Integration, collection: str, wallet_addre
 #     """Runs a blocking function in a non-blocking way"""
 #     func = functools.partial(blocking_func, *args, **kwargs) # `run_in_executor` doesn't support kwargs, `functools.partial` does
 #     return await bot.loop.run_in_executor(None, func)
+
+
+def clean_wallets(list_of_wallets):
+    clean_wallets = []
+    for wallet in list_of_wallets:
+        print(wallet)
+        print(len(wallet))
+        if len(wallet) != 42 or wallet[:2] != "0x":
+            raise Exception(f"{wallet}")
+        if wallet not in clean_wallets:
+            clean_wallets.append(wallet.lower())
+    return clean_wallets
 
 if __name__ == '__main__':
     bot.run(config["discord_bot_token"])
